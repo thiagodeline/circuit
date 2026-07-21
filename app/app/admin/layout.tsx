e4469e945eafacbc2d473be86134186226b2fd12
thiagodeline <thyagodeline@gmail.com>
2026-07-20 21:33:04 -0300

    Delete app/app directory

--- app/app/admin/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthProvider } from '@/lib/auth-context';
-
-export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
